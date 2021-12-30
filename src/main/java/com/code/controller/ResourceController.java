27fce74779ed6db05248867f1ad24e8215a16885
works-code <im.yoolee@gmail.com>
2021-12-30 13:59:51 +0900

    토큰 읽어오는 부분 추가

--- src/main/java/com/code/controller/ResourceController.java
+++ src/main/java/com/code/controller/ResourceController.java
@@ -1,13 +1,23 @@
 package com.code.controller;
 
+import com.code.util.CommonUtils;
+import com.code.vo.TokenInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+@Slf4j
 @RestController
 public class ResourceController {
 
+    @Autowired
+    private CommonUtils commonUtils;
+
     @RequestMapping("/main")
-    public String main(){
-        return "success";
+    public Object main(HttpServletRequest request){
+        TokenInfo tokenInfo = commonUtils.getAccessTokenInfo(request);
+        return tokenInfo;
     }
 }
